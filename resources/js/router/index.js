d0eee8f5fd3f63bb412b02354b5c53830a72d4e5
patelhardik0504 <hardikkpatel0504@gmail.com>
2024-07-09 21:29:25 +0530

    Fixing

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,64 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes.js'
 import store from '../store'
-  
+
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
 
 router.beforeEach((to, from, next) => {
-
-    // if (store.getters.user) {
-    //   if (to.matched.some(route => route.meta.guard === 'guest')) next({ name: 'home' })
-    //   else next();
-      
-    // } else {
-    //   if (to.matched.some(route => route.meta.guard === 'auth')) next({ name: 'login' })
-    //   else next();
-    // }
-
-    if (store.getters.user) {
-      if (to.matched.some(route => route.meta.guard === 'guest')) {
-        next({ name: 'home' });
-      } else {
-        // Check payment status
-        const { submitted, approved } = store.getters.formSubmissionState;
-        console.log('submitted...'+submitted);
-        console.log('approved...'+approved);
-
-        if (submitted && approved) {
-          next();
-        } else {
-          if (to.path !== '/form') {
-            console.log('log');
-            next('/form'); // Redirect to the payment form
-          } else {
-            next(); // Stay on the form page
-          }
-        }
-        
-      }
+  const user = store.getters.user;
+  const requiresAuth = to.matched.some(route => route.meta.guard === 'auth');
+  const requiresGuest = to.matched.some(route => route.meta.guard === 'guest');
+  
+  if (user) {
+    // Redirect authenticated users away from guest routes
+    if (requiresGuest) {
+      next({ name: 'home' });
     } else {
-      if (to.matched.some(route => route.meta.guard === 'auth')) {
-        next({ name: 'login' });
-      } else {
+      // Check payment status for authenticated users
+      const { submitted, approved } = store.getters.formSubmissionState;
+
+      if (submitted && approved) {
         next();
+      } else {
+        if (to.path !== '/form') {
+          next('/form'); // Redirect to the form page if not submitted/approved
+        } else {
+          next(); // Stay on the form page
+        }
       }
     }
-    
-    
-  
-  
-  // if (store.getters.user) {
-  //   if (to.name === 'login' || to.name === 'register') next({ name: 'home' })
-  //   else next()
-  // } else {
-
-  //   if (to.name !== 'login' && to.name !== 'register') next({ name: 'login' })
-  //   else next()
-  // }
-})
+  } else {
+    // Redirect unauthenticated users to login for protected routes
+    if (requiresAuth) {
+      next({ name: 'login' });
+    } else {
+      next();
+    }
+  }
+});
 
 export default router;
-
